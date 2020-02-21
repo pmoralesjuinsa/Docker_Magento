d2bf34b6a8e541716b87caf9d136f31a9e2d29b4
Pedro Morales <pedromorales@grupojuinsa.es>
2020-02-21 13:49:19 +0100

    funciona la imagen estática obteniendo la url de la carpeta skin del tema activo

--- app/code/local/Grupojuinsa/Pahero/Block/Paheroblock.php
+++ app/code/local/Grupojuinsa/Pahero/Block/Paheroblock.php
@@ -9,6 +9,7 @@ class Grupojuinsa_Pahero_Block_Paheroblock extends Mage_Core_Block_Template
 
     public function getTheImage()
     {
-        return $this->getSkinUrl('media/xvideosMagento.png');
+        return Mage::getSingleton('core/design_package')->getSkinBaseUrl().'images/xvideosMagento.png';
     }
+
 }
